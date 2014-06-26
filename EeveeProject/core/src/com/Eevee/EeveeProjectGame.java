14efb22dd54de955383fb2bfdf516346e8c3d93b
pikachulv400 <chuzai@hotmail.my>
2014-06-26 23:55:05 +0800

    Changed structure and create logic
    use asset loader and screens to handle rendering

--- EeveeProject/core/src/com/Eevee/EeveeProjectGame.java
+++ EeveeProject/core/src/com/Eevee/EeveeProjectGame.java
@@ -1,27 +1,26 @@
 package com.Eevee;
 
+import com.Eevee.Util.AssetLoader;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class EeveeProjectGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		AssetLoader.load();
+		//setScreen(new SplashScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void dispose() {
+		super.dispose();
+		AssetLoader.dispose();
 	}
 }
